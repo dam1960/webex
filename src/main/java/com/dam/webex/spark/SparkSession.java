3d8bc3c0be612b0853761cdd2592da3033f1b3be
david <dam1960@gmail.com>
2020-08-28 11:06:21 -0600

    More changes

--- src/main/java/com/dam/webex/spark/SparkSession.java
+++ src/main/java/com/dam/webex/spark/SparkSession.java
@@ -3,22 +3,37 @@ package com.dam.webex.spark;
 import ciscospark.Message;
 import ciscospark.Room;
 import ciscospark.Spark;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
-@Component
+@Service
 public class SparkSession {
+
+    private final RestTemplate restTemplate;
 
     private String accessToken;
 
+    public SparkSession(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
+
     public Spark buildSession() {
+
         Spark spark = Spark.builder()
                 .baseUrl(URI.create("https://api.ciscospark.com/v1"))
                 .accessToken(accessToken)
                 .build();
 
         return spark;
+    }
+
+    private String getAccessToken() {
+
+//        restTemplate.postForObject("https://webexapis.com/v1/authorize", );
+        return null;
     }
 
     public Room createRoom(Spark spark) {
